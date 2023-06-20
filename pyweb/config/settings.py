29153c47a86159712c07c07cf7e077ab6ba03de2
Jamikidu <kangjh1994@gmail.com>
2023-06-20 16:29:12 +0900

    web

--- pyweb/config/settings.py
+++ pyweb/config/settings.py
@@ -106,8 +106,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'    #로그인 성공시 루트(인덱스)로 이동
+LOGOUT_REDIRECT_URL = '/'
