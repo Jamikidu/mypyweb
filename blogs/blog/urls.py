4e67aeb38abf9867b79e308b56aba7cd4f2e9b83
Jamikidu <kangjh1994@gmail.com>
2023-06-23 14:48:48 +0900

    category

--- blogs/blog/urls.py
+++ blogs/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),    #목록
     path('<int:post_id>/', views.detail, name='detail'),    #상세페이지
-    path('post/create/', views.post_create, name='post_create')   #포스트만들기
+    path('post/create/', views.post_create, name='post_create'),   #포스트만들기
+    path('category/<str:slug>/', views.category_page, name='category_page'),   #카테고리 페이지
 ]
