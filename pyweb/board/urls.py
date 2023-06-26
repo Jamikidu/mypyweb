84615fa6b7f3a8d132a6e1b29543baf1cdf476dc
Jamikidu <kangjh1994@gmail.com>
2023-06-26 17:42:14 +0900

    페이징

--- pyweb/board/urls.py
+++ pyweb/board/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     path('question/create/', views.question_create, name='question_create'),    #질문 등록
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'), #답변 등록
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),  #질문 수정
     path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),  #질문 삭제
+    path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),    #답변 삭제
 ]
